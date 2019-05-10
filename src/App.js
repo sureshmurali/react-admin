8353bca1597c681647f6bfd8103fce5d5e16558e
Suresh Murali <suresh.murali29@gmail.com>
2019-05-10 16:42:35 +0900

    Added new component from react-admin to test. It's not really flexible.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
+// in src/App.js
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Admin, Resource,} from 'react-admin';
+import { UserList } from './user';
+import jsonServerProvider from 'ra-data-json-server';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
+const App = () => (
+  // <Admin dataProvider={dataProvider}>
+  // <Resource name="users" list={UserList} />
+  // </Admin>
+    <React.Fragment>
+      <h1>Hello 1</h1>
+      <h1>Hello 2</h1>
+    <Admin dataProvider={dataProvider}>
+    <h1>Hello 3</h1>
+      <h1>Hello 4</h1>
+    <Resource name="users" list={UserList} />
+    <h1>Hello 5</h1>
+      <h1>Hello 6</h1>
+    </Admin>
+    <h1>Hello 7</h1>
+      <h1>Hello 8</h1>
+    </React.Fragment>
+);
 
 export default App;
