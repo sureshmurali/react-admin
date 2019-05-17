b6c304f4a49db79482d4b9d7801046f52e465c58
Suresh Murali <suresh.murali29@gmail.com>
2019-05-17 12:37:18 +0900

    Added custom tag next to react-admin component

--- src/App.js
+++ src/App.js
@@ -6,22 +6,9 @@ import jsonServerProvider from 'ra-data-json-server';
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 const App = () => (
-  // <Admin dataProvider={dataProvider}>
-  // <Resource name="users" list={UserList} />
-  // </Admin>
-    <React.Fragment>
-      <h1>Hello 1</h1>
-      <h1>Hello 2</h1>
-    <Admin dataProvider={dataProvider}>
-    <h1>Hello 3</h1>
-      <h1>Hello 4</h1>
-    <Resource name="users" list={UserList} />
-    <h1>Hello 5</h1>
-      <h1>Hello 6</h1>
-    </Admin>
-    <h1>Hello 7</h1>
-      <h1>Hello 8</h1>
-    </React.Fragment>
+  <Admin dataProvider={dataProvider}>
+  <Resource name="users" list={UserList} />
+  </Admin>
 );
 
 export default App;
--- src/user.js
+++ src/user.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { List, Datagrid, TextField, EmailField } from 'react-admin';
 
-export const UserList = props => (
+export const UserList = (props) => (
+    <React.Fragment>
+    <h1>Hello</h1>
+    <div style={{borderWidth: '1px', borderColor:"black", backgroundColor: "violet"}}>
+    </div>
     <List {...props}>
         <Datagrid rowClick="edit">
             <TextField source="id" />
@@ -13,5 +17,6 @@ export const UserList = props => (
             <TextField source="website" />
             <TextField source="company.name" />
         </Datagrid>
-    </List>
+    </List> 
+    </React.Fragment>
 );
